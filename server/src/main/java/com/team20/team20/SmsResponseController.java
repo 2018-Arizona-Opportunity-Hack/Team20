498783b7526afb22ef79329904b1c7cdebf697ab
Josh Mcguigan <joshmcg88@gmail.com>
2018-10-20 16:37:44 -0700

    implement sending sms

--- server/src/main/java/com/team20/team20/SmsResponseController.java
+++ server/src/main/java/com/team20/team20/SmsResponseController.java
@@ -1,19 +1,41 @@
 package com.team20.team20;
 
+import com.nexmo.client.NexmoClient;
+import com.nexmo.client.auth.AuthMethod;
+import com.nexmo.client.auth.TokenAuthMethod;
+import com.nexmo.client.sms.SmsSubmissionResult;
+import com.nexmo.client.sms.messages.TextMessage;
 import com.team20.team20.nexmo.InboundSms;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SmsResponseController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello/{phoneNumber}")
     @ResponseBody
-    public String hello() {
+    public String hello(@PathVariable String phoneNumber) throws Exception {
         System.out.println("got a hit");
+
+        Dotenv dotenv = Dotenv.load();
+        String NEXMO_PHONE_NUMBER = dotenv.get("NEXMO_PHONE_NUMBER");
+
+        String API_KEY = dotenv.get("NEXMO_API_KEY");
+        String API_SECRET = dotenv.get("NEXMO_API_SECRET");
+
+        AuthMethod auth = new TokenAuthMethod(API_KEY, API_SECRET);
+        NexmoClient client = new NexmoClient(auth);
+        System.out.println(NEXMO_PHONE_NUMBER);
+
+        SmsSubmissionResult[] responses = client.getSmsClient().submitMessage(new TextMessage(
+                NEXMO_PHONE_NUMBER,
+                phoneNumber,
+                "Hello from api thing!"));
+        for (SmsSubmissionResult response : responses) {
+            System.out.println(response);
+        }
+
         return "hello, world";
     }
 
