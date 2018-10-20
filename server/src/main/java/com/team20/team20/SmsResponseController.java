48eae270ce8c84e5fb21bc559ad59a47343303ea
Josh Mcguigan <joshmcg88@gmail.com>
2018-10-20 15:12:50 -0700

    added two way sms routes

--- server/src/main/java/com/team20/team20/SmsResponseController.java
+++ server/src/main/java/com/team20/team20/SmsResponseController.java
@@ -6,10 +6,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SmsResponseController {
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
         System.out.println("got a hit");
         return "hello, world";
     }
+
+    @GetMapping("/api/sms/inbound")
+    @ResponseBody
+    public String inbound() {
+        System.out.println("got an inbound");
+        return "hello, world";
+    }
+
+    @GetMapping("/api/sms/receipt")
+    @ResponseBody
+    public String receipt() {
+        System.out.println("got a receipt");
+        return "hello, world";
+    }
+
 }
