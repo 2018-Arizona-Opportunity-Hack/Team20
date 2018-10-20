62cdb1c4744b1f2ff3d216926c22ddc1d4607267
Josh Mcguigan <joshmcg88@gmail.com>
2018-10-20 14:36:29 -0700

    test route

---
+++ server/src/main/java/com/team20/team20/SmsResponseController.java
@@ -0,0 +1,14 @@
+package com.team20.team20;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SmsResponseController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello, world";
+    }
+}
