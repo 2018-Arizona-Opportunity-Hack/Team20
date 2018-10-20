8cca9c333436e6c84437364c3f79ab7a291287f9
Josh Mcguigan <joshmcg88@gmail.com>
2018-10-20 13:31:43 -0700

    initial commit

---
+++ server/src/main/java/com/team20/team20/Team20Application.java
@@ -0,0 +1,12 @@
+package com.team20.team20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team20Application.class, args);
+	}
+}
