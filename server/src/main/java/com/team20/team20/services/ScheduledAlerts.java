b8d08b8464148bfa86ca9c0fd840debacc10915a
Elijah <elijah.abella@aol.com>
2018-10-21 08:10:15 -0700

    added SOS feature

--- server/src/main/java/com/team20/team20/services/ScheduledAlerts.java
+++ server/src/main/java/com/team20/team20/services/ScheduledAlerts.java
@@ -36,17 +36,19 @@ public class ScheduledAlerts {
     private final
     CommunicationRepository communicationRepository;
 
+    private final
+    EventService eventService;
+
     @Autowired
-    public ScheduledAlerts(EventRepository eventRepository, EventUserRepository eventUserRepository, UserRepository userRepository, CommunicationRepository communicationRepository) {
+    public ScheduledAlerts(EventRepository eventRepository, EventUserRepository eventUserRepository, UserRepository userRepository, CommunicationRepository communicationRepository, EventService eventService) {
         this.eventRepository = eventRepository;
         this.eventUserRepository = eventUserRepository;
         this.userRepository = userRepository;
         this.communicationRepository = communicationRepository;
+        this.eventService = eventService;
     }
 
-    @Scheduled(fixedRate = 999999999)
-    public void logTime() throws Exception {
-        int noOfDays = 7;
+    public void sendConfirmationText(int noOfDays) throws Exception{
         Calendar calendar = Calendar.getInstance();
         Date tempDate = new Date();
         calendar.setTime(tempDate);
@@ -58,9 +60,6 @@ public class ScheduledAlerts {
         calendar.add(Calendar.DAY_OF_YEAR, 1);
         Date afterDate = calendar.getTime();
         List<Event> eventList = eventRepository.findUpcomingEvents(beforeDate, afterDate);
-        System.out.println(beforeDate);
-        System.out.println(afterDate);
-        System.out.println(eventList.size());
 
         for(Event event: eventList) {
             List<EventUser> eventUsers = eventUserRepository.findByEventId(event.getId());
@@ -75,16 +74,68 @@ public class ScheduledAlerts {
 
                 sender.send(eventUser.getUser().getPhone(),
                         "Hello, " +
-                        eventUser.getUser().getName() +
-                        "! Please confirm if you are still able to volunteer with Paz De Cristo on: " +
-                        date + " at " + time +
-                        ". Respond Y / N");
+                                eventUser.getUser().getName() +
+                                "! Please confirm if you are still able to volunteer with " +
+                                eventUser.getEvent().getOrganization().getName() + " on: " +
+                                date + " at " + time +
+                                ". Respond Y / N");
 
                 Communication communication = new Communication(new Date().toString(), "SMS", null, eventUserRepository.findById(eventUser.getId()).orElse(null));
                 communicationRepository.save(communication);
             }
         }
+    }
 
+    public void sendSOS() throws Exception {
+        int noOfDays = 2;
+        Calendar calendar = Calendar.getInstance();
+        Date tempDate = new Date();
+        calendar.setTime(tempDate);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.add(Calendar.DAY_OF_YEAR, noOfDays);
+        Date beforeDate = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date afterDate = calendar.getTime();
+
+        List<Event> eventList = eventRepository.findUpcomingEvents(beforeDate, afterDate);
+        for(Event event: eventList) {
+            List<User> usersGoing = eventService.getUsersWhoSayTheyreGoing(event);
+            if(usersGoing.size() < event.getDesiredAttendees()) {
+                Iterable<User> users = userRepository.findAll();
+                SmsSender sender = new SmsSender();
+                for (User user : users) {
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+                    Date newDate = sdf.parse(event.getDate().toString());
+                    sdf = new SimpleDateFormat("MM/dd/yyyy");
+                    SimpleDateFormat sdfTime = new SimpleDateFormat("hh:mm");
+                    String date = sdf.format(newDate);
+                    String time = sdfTime.format(newDate);
+
+                    sender.send(user.getPhone(),
+                            "Hello! A volunteer event on " + date +
+                                    " at " + event.getOrganization().getName() +
+                                    " is still in need of additional volunteers! If you haven't already signed up, please consider helping by visiting: localhost:3000/#/dashboard");
+                }
+            }
+        }
+    }
+
+    @Scheduled(fixedRate = 999999999)
+    public void SevenDayConfirmation() throws Exception {
+        sendConfirmationText(7);
+    }
+
+
+    @Scheduled(fixedRate = 999999999)
+    public void ThreeDayConfirmation() throws Exception {
+        sendConfirmationText(3);
+    }
+
+    @Scheduled(fixedRate = 99999999)
+    public void TwoDaySOS() throws Exception {
+        sendSOS();
     }
 
 }
