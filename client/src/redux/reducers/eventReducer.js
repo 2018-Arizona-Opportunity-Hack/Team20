6f5d51f2aa4171634e3e96d560262da67ecec8cf
Nhan Phan <xeith1712@hotmail.com>
2018-10-20 23:15:41 -0700

    asdf

--- client/src/redux/reducers/eventReducer.js
+++ client/src/redux/reducers/eventReducer.js
@@ -1,14 +1,19 @@
 import {
-  FETCH_EVENTS
+  FETCH_EVENTS,
+  FETCH_EVENT
 } from '../actions/types';
 
 
 export default (state = [], action) => {
-  switch(action.type){
+  switch (action.type) {
     case FETCH_EVENTS:
-    return [...state, ...action.payload];
+      return [...state, ...action.payload];
+
+    case FETCH_EVENT:
+      let event = state.filter((event) => event.id === action.payload)
+      return event;
 
     default:
-    return state;
+      return state;
   }
 }
