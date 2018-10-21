66874f27e2334f9eb3ded8c406b787da206b22e2
Josh Mcguigan <joshmcg88@gmail.com>
2018-10-21 12:08:52 -0700

    Updated client files

---
+++ server/src/main/resources/static/precache-manifest.6b0d68ae024e394d7bbecb2aaa533ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4eff50ca431ac37a2edd",
+    "url": "/static/js/main.4eff50ca.chunk.js"
+  },
+  {
+    "revision": "099aff1dcb4c0513fd46",
+    "url": "/static/js/1.099aff1d.chunk.js"
+  },
+  {
+    "revision": "4eff50ca431ac37a2edd",
+    "url": "/static/css/main.4fc41a1f.chunk.css"
+  },
+  {
+    "revision": "099aff1dcb4c0513fd46",
+    "url": "/static/css/1.4cee4e2b.chunk.css"
+  },
+  {
+    "revision": "5b1476543ac9aea5f1fe150d95655712",
+    "url": "/index.html"
+  }
+];
