b6f26ff7d2417036da7ae0c663a0507be8ce4441
Elijah <elijah.abella@aol.com>
2018-10-21 01:18:26 -0700

    User response functionality corrected

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -11,7 +11,7 @@ INSERT INTO users (name, phone, organization_id) VALUES
   ('Chuck McGill', '602-123-1521', 1);
 
 INSERT INTO event (date, desired_attendees, title, organization_id) VALUES
-  ('2018-10-27T09:22:12', 20, 'Serving Dinner', 1),
+  ('2018-10-28T09:22:12', 20, 'Serving Dinner', 1),
   ('2018-10-24T09:22:12', 20, 'Serving Dinner', 1),
   ('2018-10-29T09:22:12', 20, 'Serving Dinner', 1),
   ('2018-10-11T09:22:12', 20, 'Serving Dinner', 1);
